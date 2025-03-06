c7cede75c52ab216fb3e9fbc20c8ca50f2aa10e9
Prateek Agrawal <prateek.bna@gmail.com>
2025-03-06 21:24:29 +0530

    initial setup

---
+++ client/src/App.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import { Button } from "./components/ui/button";
+import { Input } from "./components/ui/input";
+
+const App = () => {
+    return (
+        <div className="">
+            <Button className="bg-red-900 text-xl px-8 py-6">App</Button>
+            <Input />
+        </div>
+    );
+};
+
+export default App;
